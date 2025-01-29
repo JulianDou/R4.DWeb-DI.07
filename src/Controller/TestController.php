6516221becd7e152b22fa7041c9f9fed794ba48c
JulianDou <julian.doutreligne@gmail.com>
2025-01-29 17:55:01 +0100

    TP1

---
+++ src/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+/* indique où "vit" ce fichier */
+namespace App\Controller;
+
+/* indique l'utilisation du bon bundle pour gérer nos routes */
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/* le nom de la classe doit être cohérent avec le nom du fichier */
+class TestController extends AbstractController
+{
+    // L'attribute #[Route] indique ici que l'on associe la route
+    // "/" à la méthode home() pour que Symfony l'exécute chaque fois
+    // que l'on accède à la racine de notre site.
+
+    #[Route('/', name: 'home')]
+    public function home()
+    {
+        $response = $this->render('test.html.twig');
+        return $response;
+    }
+}
