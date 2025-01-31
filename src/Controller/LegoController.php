455587e8f63caadf0aef25b4185cea36d20a2f7a
JulianDou <julian.doutreligne@gmail.com>
2025-01-31 09:27:03 +0100

    TP3, debut service

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -5,21 +5,25 @@ namespace App\Controller;
 
 /* indique l'utilisation du bon bundle pour gérer nos routes */
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\LegoService;
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
-class TestController extends AbstractController
+class LegoController extends AbstractController
 {
     // L'attribute #[Route] indique ici que l'on associe la route
     // "/" à la méthode home() pour que Symfony l'exécute chaque fois
     // que l'on accède à la racine de notre site.
 
     #[Route('/', name: 'home')]
-    public function home()
+    public function home(LegoService $legoService): Response
     {
-        $response = $this->render('test.html.twig');
-        return $response;
+        $lego = $legoService->getLego();
+        return $this->render('home.html.twig', [
+            'lego' => $lego,
+        ]);
     }
 }
+
